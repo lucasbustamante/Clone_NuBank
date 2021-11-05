631ec0dda7c0d446664cfd0227b93c316938d44b
LucasBustamante <lucascostabustamante@gmail.com>
2021-11-04 23:56:41 -0300

    atualização 04112021

--- lib/screens/account_value.dart
+++ lib/screens/account_value.dart
@@ -15,79 +15,89 @@ class AccountValue extends StatelessWidget {
     actions: [
       IconButton(onPressed: (){}, icon: Icon(Icons.help_outline, color: aSecondaryTextColor,))
     ],),
-    body: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Saldo disponível',
-                    style: TextStyle(color: aSecondaryTextColor,
-                        fontWeight: FontWeight.w600, fontSize: 16)),
-                SizedBox(height: 10,),
-                Text('R\$ $Value',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 40)),
-                SizedBox(height: 50,),
-                Row(children: [
-                  Icon(Icons.account_balance),
-                  SizedBox(width: 15),
-                  Column(crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Dinheiro guardado', style: TextStyle(color: aSecondaryTextColor,)),
-                      Text('R\$ $Savings',style: TextStyle(fontWeight: FontWeight.bold,
-                          fontSize: 17))],),
-                  Container(
-                    child: Icon(Icons.arrow_forward_ios_rounded, color: aSecondaryTextColor,
-                    size: 17,),)
-                ],
-                ),
-                SizedBox(height: 40),
-                Row(
-                  children: [
-                    Icon(Icons.add_chart),
-                    SizedBox(width: 10,),
+    body: SingleChildScrollView(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Saldo disponível',
+                      style: TextStyle(color: aSecondaryTextColor,
+                          fontWeight: FontWeight.w600, fontSize: 16)),
+                  SizedBox(height: 10,),
+                  Text('R\$ $Value',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 40)),
+                  SizedBox(height: 50,),
+                  Row(children: [
+                    Icon(Icons.account_balance),
+                    SizedBox(width: 15),
                     Column(crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Rendimento total da conta', style: TextStyle(color:
-                        aSecondaryTextColor,)),
-                        Text('+R\$ $Income', style: (TextStyle(color: aValueColor,
-                            fontSize: 18, fontWeight: FontWeight.bold)),),
-
-                      ],),
-                    Icon(Icons.arrow_forward_ios_sharp, color: aSecondaryTextColor,
-                    size: 17,)
+                        Text('Dinheiro guardado', style: TextStyle(color: aSecondaryTextColor,)),
+                        Text('R\$ $Savings',style: TextStyle(fontWeight: FontWeight.bold,
+                            fontSize: 17))],),
+                    Container(
+                      child: Icon(Icons.arrow_forward_ios_rounded, color: aSecondaryTextColor,
+                      size: 17,),)
                   ],
-                ),
-                SizedBox(height: 30),],
+                  ),
+                  SizedBox(height: 40),
+                  Row(
+                    children: [
+                      Icon(Icons.add_chart),
+                      SizedBox(width: 10,),
+                      Column(crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Rendimento total da conta', style: TextStyle(color:
+                          aSecondaryTextColor,)),
+                          Text('+R\$ $Income', style: (TextStyle(color: aValueColor,
+                              fontSize: 18, fontWeight: FontWeight.bold)),),
+
+                        ],),
+                      Icon(Icons.arrow_forward_ios_sharp, color: aSecondaryTextColor,
+                      size: 17,)
+                    ],
+                  ),
+                  SizedBox(height: 30),],
+              ),
             ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            physics: BouncingScrollPhysics(),
-            child: Row(
-              children: [
-                CircleButton('Função Débito',
-                icon: Icons.smartphone,),
-                CircleButton('Depositar', icon: Icons.arrow_circle_down ),
-                CircleButton('Pagar', icon: Icons.wysiwyg), 
-                CircleButton('Transferir', icon: Icons.arrow_circle_up),
-                CircleButton('Pedir Extrato', icon: Icons.ad_units_sharp),
-                CircleButton('Cobrar', icon: Icons.account_balance_wallet_outlined)
-              ],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              physics: BouncingScrollPhysics(),
+              child: Row(
+                children: [
+                  CircleButton('Função Débito',
+                  icon: Icons.smartphone,),
+                  CircleButton('Depositar', icon: Icons.arrow_circle_down ),
+                  CircleButton('Pagar', icon: Icons.wysiwyg), 
+                  CircleButton('Transferir', icon: Icons.arrow_circle_up),
+                  CircleButton('Pedir Extrato', icon: Icons.ad_units_sharp),
+                  CircleButton('Cobrar', icon: Icons.account_balance_wallet_outlined)
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 20,),
-          Divider(thickness: 2,),
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(children: [
-              Text('Histórico', style: TextStyle(fontWeight: FontWeight.bold,
-                  fontSize: 23))
-            ],),
-          )
-    ])
+            SizedBox(height: 20,),
+            Divider(thickness: 2,),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text('Histórico', style: TextStyle(fontWeight: FontWeight.bold,
+                    fontSize: 23)),
+                SizedBox(height: 40),
+                TextField(cursorColor: aPrimaryColor,
+                  decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40))
+                  )
+                ),)
+              ],),
+            )
+      ]),
+    )
     );
 
   }
