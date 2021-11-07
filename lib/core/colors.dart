d53d7ff2e3923179c6191b5393b322a9ea5b5a07
LucasBustamante <lucascostabustamante@gmail.com>
2021-11-07 12:27:55 -0300

    correção botão search

--- lib/core/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const aPrimaryColor = Color(0xff830ad1);
-const aTextColor = Colors.black;
-const aSecondaryTextColor = Color(0xFF707682);
-const aValueColor = Color(0xff097900);
-const aContainerColor = Colors.white;
-const aBackgroundColor = Color(0xfff0f1f5);
--- lib/core/constants.dart
+++
@@ -1,4 +0,0 @@
-const Username = 'Lucas';
-const Value = '2.000,00';
-const Savings = '130,00';
-const Income = '0,00';
